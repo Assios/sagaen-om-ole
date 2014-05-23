5fec880f47e077a1133c26a7270291301efe9392
Asbjørn Steinskog <asbjorn@steinskog.me>
2014-05-24 01:47:32 +0200

    Add stuff

--- load.js
+++ load.js
@@ -1,14 +1,15 @@
 var load = {
 	preload: function() {
-	    game.stage.backgroundColor = '#1d1a4f';
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1.2.5(\'0\',\'3/0.4\')',6,6,'olee|game|load|assets|mp3|audio'.split('|'),0,{}))
+
+            //LOAD STUFF
+	        game.stage.backgroundColor = '#1d1a4f';
             game.load.image('sky', 'assets/sky.jpg');
             game.load.image('bird', 'assets/bird.png');
-	    game.load.image('ground', 'assets/ground.png');
             game.load.image('powerade', 'assets/powerade.png');
             game.load.image('poweradef', 'assets/powerade_f.png');
             game.load.image('menu', 'assets/menysky.jpg');
             game.load.audio('ah', 'assets/ah.wav');
-            game.load.audio('bgmusic', 'assets/bgmusic.wav');
             game.load.audio('mhm', 'assets/mhm.wav');  
 	},
 
